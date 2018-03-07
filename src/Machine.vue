0ef59b8a05c437296cd45d39222c797763930981
EDG\olivier.menuel <olivier.menuel@campus-numerique-in-the-alps.com>
2018-03-07 12:08:46 +0100

    composant autonome et v-for

--- src/Machine.vue
+++ src/Machine.vue
@@ -1,36 +1,41 @@
 <template>
   <div>
-    <h1>Machine {{name}}</h1>
-    <h2>{{checkedAt.toLocaleString()}}</h2>
+    <!--<h1>Machine {{name}}</h1>-->
+    <!--<h2>Dernière vérification : {{checkedAt.toLocaleString()}}</h2>-->
 
-    <h3 class="green" v-if="status">Status OK</h3>
-    <h3 class="red" v-if="!status">Status KO</h3>
+    <!--<h3 class="green" v-if="status">Status OK</h3>-->
+    <!--<h3 class="red" v-if="!status">Status KO</h3>-->
+
+    <h1>Machine {{machineData.name}}</h1>
+    <h2>Dernière vérification : {{machineData.checkedAt.toLocaleString()}}</h2>
+    <h3 class="green" v-if="machineData.status">Status OK</h3>
+    <h3 class="red" v-if="!machineData.status">Status KO</h3>
 
   </div>
 </template>
 
 <script>
   export default {
-    name: "machine",
+    name: "machineAlone",
+    props: [
+      'machineData'
+    ],
     data() {
-      return {
-
-        name: 'Good Machine',
-        status: false,
-        checkedAt: new Date(),
-
-
-      }
+      return {}
     },
 
   }
 </script>
 
 <style scoped>
-.red{
-  color: mediumvioletred;
-}
-  .green{
+  div {
+    border: medium;
+  }
+  .red {
+    color: mediumvioletred;
+  }
+
+  .green {
     color: aqua;
   }
 </style>
--- src/MachinesList.vue
+++ src/MachinesList.vue
@@ -1,11 +1,33 @@
 <template>
-<h1>Liste des machines</h1>
+  <div>
+    <h1>Liste des machines</h1>
+    <MachineAlone v-for="machine in machines"
+    :key="machine.id"
+    v-bind:machineData="machine">
+
+    </MachineAlone>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "machines-list"
-    }
+  export default {
+    name: "machines-list",
+    data() {
+      return {
+        machines: [{
+          id: 1,
+          name: 'Good Machine',
+          status: true,
+          checkedAt: new Date()
+        }, {
+          id: 2,
+          name: 'Machine in burn out',
+          status:false,
+          checkedAt: new Date()
+        }]
+      }
+    },
+  }
 </script>
 
 <style scoped>
